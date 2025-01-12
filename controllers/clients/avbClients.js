53e3e565fd7b7d5f2b74f7ed2763b645cfce093b
krysto-nc-dev <krysto.dev@gmail.com>
2025-01-13 08:54:33 +1100

    tet

--- controllers/clients/avbClients.js
+++ controllers/clients/avbClients.js
@@ -4,10 +4,19 @@ import AvbClient from "../../models/bases/AVB/Client.js";
 
 // Get all AVB Clients
 const getAvbClients = asyncHandler(async (req, res, next) => {
-  const clients = await AvbClient.find();
+  // Utiliser les résultats de `advancedResults` définis dans res.advancedResults
+  if (res.advancedResults) {
+    return res.status(200).json(res.advancedResults);
+  }
 
-  res.status(200).json({ success: true, count: clients.length, data: clients });
+  // Si `advancedResults` n'a pas été exécuté ou retourné un résultat
+  const articles = await AvbClient.find();
+
+  res
+    .status(200)
+    .json({ success: true, count: articles.length, data: articles });
 });
+
 
 // Get single AVB client
 const getAvbClient = asyncHandler(async (req, res, next) => {
